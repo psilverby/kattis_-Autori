81bae38f9d0a00a3499c913d3041f7c9c1148be1
psilverby <87539667+psilverby@users.noreply.github.com>
2021-07-16 18:36:06 +0200

    Add files via upload
    First upload to repository

---
+++ Autori/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Autori
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] names = input.Split("-");
+            string abbrevation = string.Empty;
+            foreach (var name in names)
+            {
+                abbrevation =  string.Concat(abbrevation, name.Substring(0, 1));
+            }
+
+            Console.WriteLine(abbrevation);
+        }
+    }
+}
